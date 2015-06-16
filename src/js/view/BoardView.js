051d570b9f098a703d1b9408b1fb529870f8da9d
Issame EL KHARBILI <elkharbli.issame@gmail.com>
2015-06-16 14:49:13 +0200

    add column handling functionality

--- src/js/view/BoardView.js
+++ src/js/view/BoardView.js
@@ -1,21 +1,21 @@
 define(['backbone', 'handlebars', 'model/Colonnes', 'model/Colonne', 'hbs!../templates/board'],
   function(Backbone, Handlebars, Colonnes, Colonne, template){
 
-var vueSurLaMer = Backbone.View.extend({
+var BoardView = Backbone.View.extend({
 
-  el: "#board",
-  colonnes: new Colonnes(),
+    el: "#board",
+    colonnes: new Colonnes(),
 
-  initialize: function() {
-    this.colonnes.reset();
-  },
+    initialize: function() {
+      this.colonnes.reset();
+    },
 
-  render: function() {
-    this.$el.html(template({title: "mon board"}));
-    return this;
-  }
+    render: function() {
+      this.$el.html(template({title: "mon board"}));
+      return this;
+    }
 
-});
+  });
 
-return vueSurLaMer;
+  return BoardView;
 });
--- test/view/BoardViewSpec.js
+++ test/view/BoardViewSpec.js
@@ -16,8 +16,41 @@ define(['jasmineJquery','backbone', 'boardView'], function(jasmineJquery, Backbo
 
       // Then
       expect($("#boardTitle").text()).toEqual("mon board");
-      // var $boardTitle = $(boardView.$el.find("#boardTitle"));
-      // expect($boardTitle.text()).toEqual("mon board");
+    });
+
+
+    it ("add button" , function() {
+      // Given
+
+      // When
+      boardView.render();
+
+      //then
+      expect($("#addColonne")).toBeVisible();
+
+    });
+
+    it ("should display 3 colonnes", function() {
+      // Given
+
+      // When
+      boardView.render();
+
+      //then
+      expect($(".colonne").length).toEqual(3);
+    });
+
+    it ("should display columns with titles", function() {
+      // Given
+
+      // When
+      boardView.render();
+
+      //then
+      var colonneTitle = $(".colonne > .titre");
+      expect($(colonneTitle[0]).text()).toEqual("Je sais pas");
+      expect($(colonneTitle[1]).text()).toEqual("Je sais pas");
+      expect($(colonneTitle[2]).text()).toEqual("Je suis un blaireau et je m'appelle issame");
     });
   });
 
