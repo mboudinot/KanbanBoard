bf22f43e5d36be5733dcf1201e27cb60ef146c5c
Issame EL KHARBILI <Arkenchoust@octo-IEL-2.local>
2015-06-23 14:09:53 +0200

    add/delete columns on buttons click

--- src/js/view/BoardView.js
+++ src/js/view/BoardView.js
@@ -1,10 +1,14 @@
-define(['backbone', 'handlebars', 'model/Colonnes', 'model/Colonne', 'hbs!../templates/board'],
-  function(Backbone, Handlebars, Colonnes, Colonne, template){
+define(['backbone', 'handlebars', 'model/Colonnes', 'model/Colonne', 'hbs!../templates/board', 'hbs!../templates/colonne'],
+  function(Backbone, Handlebars, Colonnes, Colonne, template, colonneTemplate){
 
 var BoardView = Backbone.View.extend({
-
     el: "#board",
     colonnes: new Colonnes(),
+
+    events: {
+      "click #addColumn": "addColumn",
+      "click #removeColumn": "removeColumn"
+    },
 
     initialize: function() {
       this.colonnes.reset();
@@ -13,6 +17,15 @@ var BoardView = Backbone.View.extend({
     render: function() {
       this.$el.html(template({title: "mon board"}));
       return this;
+    },
+
+    addColumn: function() {
+      colonneTemplate();
+      this.$('#colonneList').append(colonneTemplate());
+    },
+
+    removeColumn: function() {
+      this.$('#colonneList .colonne').last().remove();
     }
 
   });
--- test/view/BoardViewSpec.js
+++ test/view/BoardViewSpec.js
@@ -6,51 +6,52 @@ define(['jasmineJquery','backbone', 'boardView'], function(jasmineJquery, Backbo
     beforeEach(function () {
       setFixtures("<div id='trello'><div id='board'></div></div>");
       boardView = new BoardView();
+      boardView.render();
     });
 
     it("should display title",function() {
-      // Given
-
-      // When
-      boardView.render();
-
       // Then
       expect($("#boardTitle").text()).toEqual("mon board");
     });
 
+    describe ("addColumn", function(){
+      it ("show addColumn button" , function() {
+        // Then
+        expect($("#addColumn")).toBeVisible();
 
-    it ("add button" , function() {
-      // Given
+      });
 
-      // When
-      boardView.render();
+      it ("click on button adds Column in board view" , function() {
+        //When
+        $("#addColumn").click();
 
-      //then
-      expect($("#addColonne")).toBeVisible();
+        // Then
+        expect($("#colonneList .colonne").length).toEqual(4);
+      });
 
     });
 
+    describe("removeColumn", function() {
+      it ("should remove column when clicked" , function() {
+        //When
+        $("#removeColumn").click();
+
+        // Then
+        expect($("#colonneList .colonne").length).toEqual(2);
+      });
+    });
+
     it ("should display 3 colonnes", function() {
-      // Given
-
-      // When
-      boardView.render();
-
-      //then
+      // Then
       expect($(".colonne").length).toEqual(3);
     });
 
     it ("should display columns with titles", function() {
-      // Given
-
-      // When
-      boardView.render();
-
-      //then
+      // Then
       var colonneTitle = $(".colonne > .titre");
       expect($(colonneTitle[0]).text()).toEqual("Je sais pas");
       expect($(colonneTitle[1]).text()).toEqual("Je sais pas");
-      expect($(colonneTitle[2]).text()).toEqual("Je suis un blaireau et je m'appelle issame");
+      expect($(colonneTitle[2]).text()).toEqual("Je suis un blaireau!!!");
     });
   });
 
