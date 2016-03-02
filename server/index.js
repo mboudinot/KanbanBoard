184ca9406fc5f57c2eb306486f199dfdcbc60fd8
Francoise Lasserre <fran.m.lasserre@gmail.com>
2016-03-02 10:00:45 +0100

    Suppression de RAZ inutile avant choix de couleur

--- server/index.js
+++ server/index.js
@@ -8,18 +8,17 @@ var port = server.address().port;
 
 colors = {
             green: function (str) {
-                return "\033[0m\033[32m" + str + "\033[0m"
+                return "\033[32m" + str + "\033[0m"
             },
             red: function (str) {
-                return "\033[0m\033[31m" + str + "\033[0m"
+                return "\033[31m" + str + "\033[0m"
             },
             yellow: function (str) {
-                return "\033[0m\033[33m" + str + "\033[0m"
+                return "\033[33m" + str + "\033[0m"
             }
-          }
+         }
 
-
-console.log("\n  " + colors.yellow("Dojo Team.\n  2015, Trello Like.\n") + "  ------------\n")
+console.log("\n  " + colors.yellow("Dojo Team.\n  2015, Trello Like.\n") + "  ------------------\n")
 console.log(colors.green("  Server running on port ") + colors.red(port) + "\n")
 console.log(colors.yellow("  Par ordre d'apparition : \n  Eric Ondoa\n  Issame El Kharbili\n  Boris Monti\n  Matthieu Boudinot\n  Boliang Tan\n  Jamal Zidane\n  Françoise Lasserre\n  Alexis Mauroy\n  José Cardoso\n  Dominique Joisin") + "\n")
 
